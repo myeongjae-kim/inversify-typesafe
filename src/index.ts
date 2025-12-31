db6fe530b950dd3d43173beb73b19b970d22d565
Myeongjae Kim (김명재) <dev@myeongjae.kim>
2025-12-31 10:26:54 +0900

    chore: elaborate type description

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
 import { BindToFluentSyntax, Container, ContainerOptions, inject, ServiceIdentifier } from "inversify";
 
-export type TypesafeContainer<S extends Record<string, unknown>> =
-  Omit<Container, "get"> & {
-    get<T extends keyof S>(serviceName: T | Exclude<ServiceIdentifier<T>, string>): S[T];
-  };
+type AbstractServiceMap = Record<string, unknown>;
 
-export type TypesafeServiceConfig<S extends Record<string, unknown>> = {
-  [K in keyof S]: ((
-    bind: BindToFluentSyntax<S[K]>,
-    container: TypesafeContainer<S>
-  ) => void);
+export type TypesafeContainer<S extends AbstractServiceMap> =
+  Omit<Container, "get"> & { get<T extends keyof S>(serviceName: T | Exclude<ServiceIdentifier<T>, string>): S[T]; };
+
+export type TypesafeServiceConfig<S extends AbstractServiceMap> = {
+  [K in keyof S]: ((bind: BindToFluentSyntax<S[K]>, container: TypesafeContainer<S>) => void);
 };
 
-export const returnTypesafeInject = <S extends Record<string, unknown>>() => (name: Extract<keyof S, string>) => inject(name);
+export const returnTypesafeInject = <S extends AbstractServiceMap>() => (name: Extract<keyof S, string>) => inject(name);
 
-export const createTypesafeContainer = <S extends Record<string, unknown>>(
+export const createTypesafeContainer = <S extends AbstractServiceMap>(
   serviceConfig: TypesafeServiceConfig<S>,
   options?: ContainerOptions,
 ): TypesafeContainer<S> => {
