f4df38b94a058afe08a54d7633ce3c59919450da
Myeongjae Kim (김명재) <dev@myeongjae.kim>
2025-12-31 17:46:14 +0900

    feat: disallow ServiceIdentifier<T> as parameter

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { BindToFluentSyntax, Container, ContainerOptions, inject, ServiceIdentifier } from "inversify";
+import { BindToFluentSyntax, Container, ContainerOptions, inject } from "inversify";
 
 type AbstractServiceMap = Record<string, unknown>;
 
 export type TypesafeContainer<S extends AbstractServiceMap> =
-  Omit<Container, "get"> & { get<T extends keyof S>(serviceName: T | Exclude<ServiceIdentifier<T>, string>): S[T]; };
+  Omit<Container, "get"> & { get<T extends keyof S>(serviceName: T): S[T]; };
 
 export type TypesafeServiceConfig<S extends AbstractServiceMap> = {
   [K in keyof S]: ((bind: BindToFluentSyntax<S[K]>, container: TypesafeContainer<S>) => void);
