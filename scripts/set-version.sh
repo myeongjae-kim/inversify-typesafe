f726dadc735e1eda4ee9db4a7f093ad568a789da
Myeongjae Kim <dev@myeongjae.kim>
2026-01-17 21:10:28 +0900

    chore: update version

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -9,7 +9,18 @@ VERSION=$1
 cat package.json | jq ".version = \"$VERSION\"" > package.json.tmp
 rm package.json && mv package.json.tmp package.json
 
+# set jsr.json version
+cat jsr.json | jq ".version = \"$VERSION\"" > jsr.json.tmp
+rm jsr.json && mv jsr.json.tmp jsr.json
+
 files=$(find packages -type f -name "package.json");
+
+for file in $files; do
+  cat "$file" | jq ".version = \"$VERSION\"" | jq ".dependencies.\"inversify-typesafe\" = \"^$VERSION\"" > "$file.tmp"
+  rm "$file" && mv "$file.tmp" "$file"
+done
+
+files=$(find packages -type f -name "jsr.json");
 
 for file in $files; do
   cat "$file" | jq ".version = \"$VERSION\"" | jq ".dependencies.\"inversify-typesafe\" = \"^$VERSION\"" > "$file.tmp"
