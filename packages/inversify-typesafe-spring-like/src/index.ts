2313d685cee241fd1f3d67bd08ff5fd5fa4df293
Myeongjae Kim <dev@myeongjae.kim>
2026-01-17 21:14:07 +0900

    chore: add explicit type

--- packages/inversify-typesafe-spring-like/src/index.ts
+++ packages/inversify-typesafe-spring-like/src/index.ts
@@ -1,9 +1,10 @@
 import { ContainerOptions } from "inversify";
-import { TypesafeServiceConfig as BeanConfig, createTypesafeContainer, returnTypesafeInject } from "inversify-typesafe";
+import { TypesafeServiceConfig as BeanConfig, createTypesafeContainer, returnTypesafeInject, TypesafeContainer } from "inversify-typesafe";
 
-export const returnAutowired = <S extends Record<string, unknown>>() => ({ Autowired: returnTypesafeInject<S>() });
+export const returnAutowired = <S extends Record<string, unknown>>(): { Autowired: ReturnType<typeof returnTypesafeInject<S>> } =>
+  ({ Autowired: returnTypesafeInject<S>() });
 
 export type { BeanConfig };
 
-export const ApplicationContext = <S extends Record<string, unknown>>(beanConfig: BeanConfig<S>, options?: ContainerOptions) =>
+export const ApplicationContext = <S extends Record<string, unknown>>(beanConfig: BeanConfig<S>, options?: ContainerOptions): TypesafeContainer<S> =>
   createTypesafeContainer(beanConfig, { defaultScope: "Singleton", ...options });
